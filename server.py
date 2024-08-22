515c0fe70518207cf1ee69a11cd05bf218755bdb
Yaphet Gebrecristos <yaphet10@gmail.com>
2024-08-21 20:06:55 -0400

    formatting server.py

--- server.py
+++ server.py
@@ -18,7 +18,6 @@ def get_weather():
     weather_data = get_current_weather(city)
 
     # City is not found by API
-
     if not weather_data['cod'] == 200:
         return render_template('city-not-found.html')
 
